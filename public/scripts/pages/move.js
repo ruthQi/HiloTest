a017a89eb66262763c94fe72a2c3d88e03695fb9
qiru <bjqiru@corp.netease.com>
2018-10-29 20:17:32 +0800

    添加动画

---
+++ public/scripts/pages/move.js
@@ -0,0 +1,239 @@
+class Move{
+   constructor(sun2Sprite, sun1Sprite, callBack){
+      this.startX = this.startY = this.moveX = this.moveY = null;
+      this.previousPinchScale = 1;
+      this.longTapTimeout = null;
+      this.sun2Sprite = sun2Sprite;
+      this.sun1Sprite = sun1Sprite;
+      this.callBack = callBack;
+      this.x = 0;
+      this.y = 0;
+      this.pinch = 1;
+      this.angle = 0;
+      this.width = 250;
+      this.initWidth = 250
+      this.bindEvent();
+   }
+   _getTime() {
+      return new Date().getTime();
+   }
+   _getDistance(xLen, yLen) {
+      return Math.sqrt(xLen * xLen + yLen * yLen);
+   }
+   /**
+    * 获取向量的旋转方向
+    */
+   _getRotateDirection(vector1, vector2) {
+      return vector1.x * vector2.y - vector2.x * vector1.y;
+   }
+   _getRotateAngle(vector1, vector2) {
+      let direction = this._getRotateDirection(vector1, vector2);
+      direction = direction > 0 ? -1 : 1;
+      let len1 = this._getDistance(vector1.x, vector1.y);
+      let len2 = this._getDistance(vector2.x, vector2.y);
+      let mr = len1 * len2;
+      if (mr === 0) return 0;
+      let dot = vector1.x * vector2.x + vector1.y * vector2.y;
+      let r = dot / mr;
+      if (r > 1) r = 1;
+      if (r < -1) r = -1;
+      return Math.acos(r) * direction * 180 / Math.PI;
+   }
+
+   _onTouchStart(e) {
+      if ($(".makesure").addClass("cur"), window.stage) {
+         let point = e.touches ? e.touches[0] : e;
+         let redsun = window.stage.getChildById("redsun");
+         this.x = redsun.x;
+         this.y = redsun.y;
+         this.startX = point.pageX;
+         this.startY = point.pageY;
+         window.clearTimeout(this.longTapTimeout);
+         //两点接触
+         if (e.touches.length > 1) {
+            let point2 = e.touches[1];
+            let xLen = Math.abs(point2.pageX - this.startX);
+            let yLen = Math.abs(point2.pageY - this.startY);
+            this.touchDistance = this._getDistance(xLen, yLen);
+            this.touchVector = {
+               x: point2.pageX - this.startX,
+               y: point2.pageY - this.startY
+            };
+         } else {
+            this.startTime = this._getTime();
+            this.longTapTimeout = setTimeout(() => {
+               //this._emitEvent('onLongPress');
+            }, 800);
+            if (this.previousTouchPoint) {
+               if (Math.abs(this.startX - this.previousTouchPoint.startX) < 10 &&
+                  Math.abs(this.startY - this.previousTouchPoint.startY) < 10 &&
+                  Math.abs(this.startTime - this.previousTouchTime) < 300) {
+                  //this._emitEvent('onDoubleTap');
+               }
+            }
+            this.previousTouchTime = this.startTime;
+            this.previousTouchPoint = {
+               startX: this.startX,
+               startY: this.startY
+            };
+         }
+         $(".shoushi").hide()
+      }
+      
+   }
+   _onTouchMove(e) {
+      if(window.stage){
+         let timestamp = this._getTime();
+         if (e.touches.length > 1) {
+            let xLen = Math.abs(e.touches[0].pageX - e.touches[1].pageX);
+            let yLen = Math.abs(e.touches[1].pageY - e.touches[1].pageY);
+            let touchDistance = this._getDistance(xLen, yLen);
+            if (this.touchDistance) {
+               let pinchScale = touchDistance / this.touchDistance;
+               //this._emitEvent('onPinch', { scale: pinchScale - this.previousPinchScale });
+               this.onPinch({
+                  pinch: pinchScale - this.previousPinchScale
+               })
+               this.previousPinchScale = pinchScale;
+            }
+            if (this.touchVector) {
+               let vector = {
+                  x: e.touches[1].pageX - e.touches[0].pageX,
+                  y: e.touches[1].pageY - e.touches[0].pageY
+               };
+               let angle = this._getRotateAngle(vector, this.touchVector);
+               this.touchVector.x = vector.x;
+               this.touchVector.y = vector.y;
+            }
+         } else {
+            window.clearTimeout(this.longTapTimeout);
+            let point = e.touches ? e.touches[0] : e;
+            let deltaX = this.moveX === null ? 0 : point.pageX - this.moveX;
+            let deltaY = this.moveY === null ? 0 : point.pageY - this.moveY;
+            this.onMove({
+               deltaX,
+               deltaY
+            })
+            this.moveX = point.pageX;
+            this.moveY = point.pageY;
+         }
+         e.preventDefault();
+      }
+      
+   }
+   _onTouchCancel(e) {
+      window.stage && this._onTouchEnd(e);
+   }
+   _onTouchEnd(e) {
+      /**
+       * 在X轴或Y轴发生过移动
+       */
+      if (window.stage) {
+         window.clearTimeout(this.longTapTimeout);
+         let timestamp = this._getTime();
+         if (this.moveX !== null && Math.abs(this.moveX - this.startX) > 10 ||
+            this.moveY !== null && Math.abs(this.moveY - this.startY) > 10) {
+            if (timestamp - this.startTime < 500) {
+               this.onSwipe();
+            }
+         } else if (timestamp - this.startTime < 2000) {
+            if (timestamp - this.startTime < 500) {
+               this.onTap();
+            }
+         }
+         this.startX = this.startY = this.moveX = this.moveY = null;
+         this.width = this.width * this.previousPinchScale;
+         console.log('-------------------------------------------', this.width, this.previousPinchScale)
+         this.previousPinchScale = 1;
+         this.width = this.initWidth * this.pinch
+         console.log('-------------------------------------------', this.initWidth, this.pinch)
+      }
+   }
+   bindEvent() {
+      $("#filter").on("touchstart", this._onTouchStart.bind(this));
+      $("#filter").on("touchmove", this._onTouchMove.bind(this));
+      $("#filter").on("touchcancel", this._onTouchCancel.bind(this));
+      $("#filter").on("touchend", this._onTouchEnd.bind(this));
+   }
+   onPinch(t) {
+      let pinch = this.pinch + t.pinch;
+      //this.pinch += t.pinch;
+      let redsun = window.stage.getChildById("redsun");
+      redsun.scaleX = this.pinch;
+      redsun.scaleY = this.pinch;
+      if (pinch > 530 / this.initWidth){
+         this.pinch = 530 / this.initWidth
+      }else{
+         if (pinch < 0.2){
+            this.pinch = .2
+         }else{
+            this.pinch = pinch;
+         }
+      }
+   }
+   onRotate(t) {
+      let angle = this.angle + t.angle;
+      let redsun = window.stage.getChildById("redsun");
+      redsun.rotation = angle;
+      this.angle = angle
+   }
+   onMove(t) {
+      let x = this.x + t.deltaX,
+         y = this.y + t.deltaY,
+         redsun = window.stage.getChildById("redsun");
+      redsun.x = x;
+      redsun.y = y;
+      if(x <= 0){
+         this.x = 0
+      }else{
+         if(x + this.width > 750){
+            this.x = 750 - this.width;
+         }else{
+            this.x = x;
+         }
+      }
+      if(y <= 0){
+         this.y = 0;
+      }else{
+         if(y + this.width > 530){
+            this.y = 530 - this.width;
+         }else{
+            this.y = y;
+         }
+      }
+   }
+   onTap(){
+
+   }
+   onSwipe(){
+
+   }
+   getRelationship = function (t) {
+      let halfX = Math.round(this.x + t / 2),//e
+         halfY = Math.round(this.y + t / 2),//n
+         half = Math.round(t / 2),//i
+         sun2halfX = Math.round(this.sun2Sprite.x + this.sun2Sprite.width / 2),//o
+         sun2halfY = Math.round(this.sun2Sprite.y + this.sun2Sprite.width / 2),//a
+         sun2Half = Math.round(this.sun2Sprite.width / 2),//s
+         length = Math.sqrt(Math.pow(Math.abs(sun2halfX - halfX), 2) + Math.pow(Math.abs(sun2halfY - halfY), 2));//c
+      console.log("X1:" + halfX + ",Y1:" + halfY + ",R1:" + half + ",X2:" + sun2halfX + ",Y2:" + sun2halfY + ",R2:" + sun2Half + ",length:" + length);
+      let type = 'normal';
+      if(length > half + sun2Half){
+         type = 'poor';
+      }else{
+         if (length < half + sun2Half && length > Math.abs(sun2Half - half)){
+            type = 'normal';
+         } else {
+            if(t > this.initWidth){
+               type = 'rich';
+            }else{
+               type = 'normal'
+            }
+         }
+      }
+      console.log('type: ' + type)
+      this.callBack(type)
+   }
+}
+
+export default Move;
--- public/scripts/pages/rich.js
+++ public/scripts/pages/rich.js
@@ -1,5 +1,6 @@
 import { data, resultData, imgSrc} from './data.js';
 import TWEEN from '@tweenjs/tween.js'
+import Move from './move';
 
 class Rich{
    constructor(){
@@ -11,12 +12,15 @@ class Rich{
       this.dialogCount = $('#dialog_cont');
       this.width = window.innerWidth;
       this.height = window.innerHeight;
-      this.poorPercent = 0;
-      this.richPercent = 0;
+      this.poorPercent = 0;//_
+      this.richPercent = 0;//y
+      this.realPoorPercent = 0;//x
+      this.realRichPercent = 0;//T
       this.resultImg;
       this.scrollTo();
       this.loadScene();
       this.update();
+      this.bindEvent();
       
    }
    scrollTo(){
@@ -70,6 +74,55 @@ class Rich{
       TWEEN.update();
       requestAnimationFrame(() => { this.update() });
    }
+   bindEvent(){
+      $("#dialog_cont").one("click", ".start_btn", (e)=>{
+         console.log(e)
+         let target = e.target;
+         let ques = $(target).attr('quse-attr');
+         let next = $(target).attr('next-attr');
+         this.addScene(data[ques][next])
+      });
+      $("#dialog_cont").on("touchend", ".options", (e) => {
+         let currentTar = e.currentTarget
+         let $option = $(currentTar).find('.option');
+         let manAttr = $option.attr('man-attr');
+         let quse = $option.attr('quse-attr');
+         let nextArr = $option.attr('next-attr');
+         if (!$(this).parent().hasClass('hasclick')){
+            $option.addClass("cur");
+            $(this).addClass("cur");
+            $(this).parent().addClass("hasclick");
+            this.computedNum(manAttr);
+            this.addScene(data[quse][nextArr]);
+         }
+
+      })
+      $("#dialog_cont").one("click", ".makesure.cur",  (t) => {
+         console.log('999999999')
+         $(".shoushi").hide();
+         //console.log(this.move)
+         this.move.getRelationship(this.move.width);
+         this.sun1Sprite.play();
+         this.sun2Sprite.play();
+         setTimeout( () => {
+            this.addScene(data[3][4])
+         }, 2e3)
+      })
+   }
+   computedNum(manType){
+      switch (manType) {
+         case 'rich':
+            this.richPercent += 1;
+            break;
+         case 'normal':
+           this.richPercent += .5;
+           this.poorPercent += .5;
+           break;
+         case 'poor':
+            this.poorPercent += 1;
+      }
+      console.log(this.poorPercent + ';' + this.richPercent)
+   }
    addScene(obj){
       switch (obj.type) {
          case "chatQ": 
@@ -88,8 +141,8 @@ class Rich{
             this.dialogCount.append(obj.content);
             break;
          case "canvas":
-            this.dialogCount.append(t.content);
-             //s();
+            this.dialogCount.append(obj.content);
+            this.loadCanvas();
             break;
          case "tip":
             this.dialogCount.append(obj.content);
@@ -118,10 +171,295 @@ class Rich{
          }, obj.timer)
       }
    }
-   loadEnd(){//o()
+   loadCanvas(){//s()
+      let canvas = $("canvas")[0];
+      let width = 750;
+      let height = 530;
+      canvas.setAttribute('width', width);
+      canvas.setAttribute('height', height);
+      let stage = window.stage = new Hilo.Stage({
+         canvas,
+         width,
+         height
+      });
+      let tickTime = new Hilo.Ticker(30);
+      tickTime.addTick(stage);
+      tickTime.addTick(Hilo.Tween);
+      tickTime.start();
+      stage.enableDOMEvent(Hilo.event.POINTER_START, !0);
+      stage.enableDOMEvent(Hilo.event.POINTER_MOVE, !0);
+      stage.enableDOMEvent(Hilo.event.POINTER_END, !0);
 
+      let redsun1Texture = new Hilo.TextureAtlas({
+         id: "redsun1_atlas",
+         image: "images/pages/circle4.png",
+         width: 250,
+         height: 250,
+         frames: [
+            [10, 8, 250, 250],
+            [280, 8, 250, 250],
+            [550, 8, 250, 250],
+            [820, 8, 250, 250],
+            [1090, 8, 250, 250],
+            [1360, 8, 250, 250],
+            [1630, 8, 250, 250],
+            [10, 278, 250, 250],
+            [280, 278, 250, 250],
+            [550, 278, 250, 250],
+            [820, 278, 250, 250],
+            [1090, 278, 250, 250]
+         ],
+         sprites: {
+            redsun1: {
+               from: 0,
+               to: 11
+            }
+         }
+      });
+      this.sun1Sprite = new Hilo.Sprite({
+         id: "redsun",
+         frames: redsun1Texture.getSprite("redsun1"),
+         x: 400,
+         y: 148,
+         interval: 50,
+         timeBased: !0,
+         loop: !1,
+         paused: !0
+      }).addTo(stage);
+
+      let redsun2Texture = new Hilo.TextureAtlas({
+         id: "redsun2_atlas",
+         image: "images/pages/circle3.png",
+         width: 250,
+         height: 250,
+         frames: [
+            [11, 8, 250, 250],
+            [281, 8, 250, 250],
+            [551, 8, 250, 250],
+            [821, 8, 250, 250],
+            [1091, 8, 250, 250],
+            [1361, 8, 250, 250],
+            [1631, 8, 250, 250],
+            [11, 278, 250, 250],
+            [281, 278, 250, 250],
+            [551, 278, 250, 250],
+            [821, 278, 250, 250],
+            [1091, 278, 250, 250],
+            [1361, 278, 250, 250]
+         ],
+         sprites: {
+            redsun2: {
+               from: 0,
+               to: 12
+            }
+         }
+      });
+
+      this.sun2Sprite = new Hilo.Sprite({
+         id: "redsun2",
+         frames: redsun2Texture.getSprite("redsun2"),
+         x: 100,
+         y: 148,
+         interval: 50,
+         timeBased: !0,
+         loop: !1,
+         paused: !0
+      }).addTo(stage);
+      this.move = new Move(this.sun2Sprite, this.sun1Sprite, (type)=>{
+         this.computedNum(type)
+      });
    }
-
+   loadEnd(){//o()
+      this.realPoorPercent = parseFloat((this.poorPercent / 6 * 100).toFixed(2));
+      this.realRichPercent = parseFloat(100 - this.realPoorPercent);
+      let arr = [-2, -1, 0, 1, 2];
+      if (this.realPoorPercent == 0 || this.realPoorPercent == 50 || this.realPoorPercent == 100){
+         this.poorPercent = Math.round(this.realPoorPercent);
+         this.richPercent = parseFloat(100 - this.poorPercent);
+      }else{
+         let round = arr[Math.floor(Math.random() * arr.length)];
+         this.poorPercent = Math.round(this.realPoorPercent + round);
+         this.richPercent = parseFloat(100 - this.poorPercent)
+      }
+      console.log("poorPercent:" + this.poorPercent + ",realPoorPercent:" + this.realPoorPercent);
+      let num;
+      if (this.realPoorPercent){
+         num = 11;
+      } else if (this.realPoorPercent > 83 && this.realPoorPercent < 92){
+         num = 10;
+      }else if( this.realPoorPercent == 75){
+         num = 9;
+      }else if( this.realPoorPercent == 66.67){
+         num = 8;
+      }else if( this.realPoorPercent == 58.33){
+         num = 7;
+      }else if( this.realPoorPercent == 50){
+         num = 6;
+      }else if(this.realPoorPercent == 41.67){
+         num = 5;
+      }else if( this.realPoorPercent == 33.33){
+         num = 4;
+      }else if(this.realPoorPercent == 25){
+         num = 3;
+      }else if(this.realPoorPercent == 16.67){
+         num ==2;
+      }else if(this.realPoorPercent == 8.33){
+         num = 1;
+      }else if(this.realPoorPercent == 0){
+         num = 0
+      }
+      this.showResult(num);
+   }
+   showResult(num){
+      console.log(num);
+      let height = this.height < 1206 ? 1206 : this.height;
+      var canvas = Hilo.createElement("canvas", {
+         width: this.width,
+         height: height
+      });
+      $("#myCanvas").append(canvas);
+      let stage = new Hilo.Stage({
+         canvas: canvas,
+         width: this.width,
+         height: height
+      });
+      stage.enableDOMEvent(Hilo.event.POINTER_START, !0);
+      stage.enableDOMEvent(Hilo.event.POINTER_MOVE, !0);
+      stage.enableDOMEvent(Hilo.event.POINTER_END, !0);
+      let tick = new Hilo.Ticker(60);
+      tick.addTick(Hilo.Tween);
+      tick.addTick(stage);
+      tick.start();
+      if(this.height < 1300){
+         var conHeight = 1206,
+            conY = 0,
+            imageR = "images/pages/r1206_2.jpg",
+            wordY = 706,
+            desY = 770,
+            bgImage = "images/pages/r1206_100_3.jpg",
+            poorPercentY = 240
+      }else{
+         var conHeight = 1334,
+            conY = (height-1334)/2,
+            imageR = "images/pages/r1334_2.jpg",
+            wordY = 766,
+            desY = 830,
+            bgImage = "images/pages/r1334_100_3.jpg",
+            poorPercentY = 280
+      }
+      let stageCon = (new Hilo.Container({
+         id: "stage_bg",
+         children: [],
+         width: this.width,
+         height: height,
+         background: "#fff",
+         x: 0,
+         y: 0
+      }).addTo(stage),
+      new Hilo.Container({
+         id: "resultC",
+         children: [],
+         width: this.width,
+         height: conHeight,
+         background: "#fff",
+         x: 0,
+         y: conY
+      }).addTo(stage));
+      new Hilo.Bitmap({
+         id: "resultC_bg",
+         image: imageR,
+         rect: [0, 0, 750, conHeight],
+         x: 0,
+         y: 0
+      }).addTo(stageCon);
+      let resultObj = resultData[num];
+      if ("" != resultObj.name) {
+         new Hilo.Text({
+            id: "nishi",
+            color: "#444444",
+            font: "28px/50px PingFangSC-Regular, sans-serif",
+            lineSpacing: 2,
+            outline: !1,
+            height: "52",
+            textVAlign: "bottom",
+            maxWidth: 400,
+            text: "你是",
+            x: 70,
+            y: wordY
+         }).addTo(stageCon);
+         new Hilo.Text({
+            id: "chengwei",
+            color: "#c3a26d",
+            font: "36px/50px PingFangSC-Regular, sans-serif",
+            lineSpacing: 2,
+            outline: !1,
+            text: resultObj.name,
+            x: 130,
+            y: wordY - 6,
+            maxWidth: 400
+         }).addTo(stageCon);
+         new Hilo.Text({
+            id: "nishi",
+            color: "#444444",
+            font: "28px/50px PingFangSC-Regular, sans-serif",
+            lineSpacing: 2,
+            outline: !1,
+            maxWidth: 400,
+            text: resultObj.title,
+            x: 130 + 36 * resultObj.name.length,
+            y: wordY
+         }).addTo(stageCon);
+         new Hilo.Text({
+            id: "nishi",
+            color: "#444444",
+            font: " 28px/56px PingFangSC-Regular, sans-serif",
+            lineSpacing: 2,
+            outline: !1,
+            maxWidth: 620,
+            text: resultObj.des,
+            pivotX: 0,
+            pivotY: 0,
+            x: 70,
+            y: desY
+         }).addTo(stageCon)
+      } else {
+         new Hilo.Bitmap({
+            id: "result11_bg",
+            image: bgImage,
+            rect: [0, 0, 750, conHeight],
+            x: 0,
+            y: 0
+         }).addTo(stageCon)
+      }
+      new Hilo.Text({
+         id: "poorPercentText",
+         color: "#666666",
+         font: "bold 80px/86px PingFangSC-Regular, sans-serif",
+         lineSpacing: 2,
+         outline: !1,
+         text: this.poorPercent,
+         textAlign: "right",
+         width: 145,
+         x: 120,
+         y: poorPercentY
+      }).addTo(stageCon);
+      new Hilo.Text({
+         id: "richPercentText",
+         color: "#c3a26d",
+         font: "bold 80px/86px PingFangSC-Regular, sans-serif",
+         lineSpacing: 2,
+         outline: !1,
+         text: this.richPercent,
+         textAlign: "right",
+         width: 145,
+         x: 383,
+         y: poorPercentY
+      }).addTo(stageCon);
+      setTimeout( () => {
+         this.resultImg = canvas.toDataURL("image/jpeg");
+         $("#bigImg").attr("src", this.resultImg);
+      }, 2e3)
+      
+   }
 }
-
 new Rich();
